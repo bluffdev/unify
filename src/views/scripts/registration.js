987369c03239fcb40c5541e71a3e2b0c864afdc1
Dylan roo <dreuterpro@gmail.com>
2022-03-02 17:30:27 -0500

    Finished Registration

--- src/views/scripts/registration.js
+++ src/views/scripts/registration.js
@@ -1,31 +1,37 @@
-import axios from 'axios'
 
-const handleSubmit = async() => {
-    // store the states in the form data
-    email = document.getElementById("emailField")
-    username = document.getElementById("usernameField")
-    password = document.getElementById("passwordField")
+//let submitButton = document.getElementById('submitButton');
+//submitButton.addEventListener("click", register());
 
-    try {
-      // make axios post request
-      const response = await axios({
-        method: "post",
-        url: "/register",
-        data: {
-            email: email,
-            username: username,
-            password: password
-        },
-        headers: { "Content-Type": "json" },
-      });
-    } catch(error) {
-      console.log(error)
-    }
+function register() {
+
+  let email = document.getElementById('emailField').value;
+  let username = document.getElementById('usernameField').value;
+  let password = document.getElementById('passwordField').value;
+
+  /* Data which will be sent to server */
+  let postObj = { 
+    username: username,
+    password: password,
+    email: email
+  }
+
+  let post = JSON.stringify(postObj)
+  
+  const url = "/register"
+  let xhr = new XMLHttpRequest()
+  
+  xhr.open('POST', url, true)
+  xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8')
+  xhr.send(post);
+  
+  xhr.onload = function () {
+      if(xhr.status === 201) {
+          console.log("Post successfully created!") 
+      }
+  }  
 }
 
-let button = document.getElementById('submitButton');
 
-button.addEventListener('click', handleSubmit);
 
 
 
